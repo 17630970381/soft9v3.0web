924a3562777c4b031accb336b4cbed066cf23269
WQ <lukstr_w@163.com>
2023-06-01 02:00:56 +0800

    添加了症状tag，能和选项卡互相影响

--- src/components/user/Predict.vue
+++ src/components/user/Predict.vue
@@ -11,18 +11,40 @@
     
     <el-divider></el-divider>
 
-    <el-main id="select">
-        <el-collapse v-model="symptom.activeNames" >
-            <el-collapse-item v-for="(p) of symptom.part" :key="p.index" :title="`${p.name} ${p.index}`">
-                <span v-for="(symp) of p.symp" :key="symp.index">
+    <!-- -------------------------------------症状选择页面---------------------------------------------- -->
+    <el-main  v-show="symptom.isShow">
+        <el-collapse v-model="symptom.activeNames" id="select">
+            <el-collapse-item v-for="(p,index1) of symptom.part" :key="p.code" :title="`${p.name} ${p.code}`">
+                <span v-for="(symp,index2) of p.symp" :key="symp.code">
                     <span class="partName">{{symp.name}}</span>
                     <el-switch 
-                    v-model="symp.isGet" 
+                    v-model="symp.isGet"
+                    @change="changeGetted(index1,index2)" 
                     ></el-switch>
                 </span>
             </el-collapse-item>
+            <div style="height: 50px;">
+                <el-tag
+                    class="tag"
+                    v-for="tag in symptom.getted"
+                    :key="tag.code"
+                    closable
+                    @close="delTag(tag.code)"
+                    >
+                    {{tag.name}}
+                </el-tag>
+            </div>
         </el-collapse>
+        <el-button id="next" type="primary" @click="submitPredict" style="margin-left: 45%;margin-top: 50px;">
+            提交预测
+        </el-button>
     </el-main>
+
+    <!-- -------------------------------------疾病预测页面---------------------------------------------- -->
+    <el-main v-show="predict.isShow">
+
+    </el-main>
+    
   </div>
 </template>
 
@@ -34,122 +56,157 @@ export default {
             step: 1,
             symptom: {
                 isShow: true,
-                activeNames: ['1'],
                 part:[{
-                        index:'face',
+                        code:'face',
                         name:'五官',
                         symp:[{
-                            index:'puffy_face_and_eyes',
+                            code:'puffy_face_and_eyes',
                             name:'脸和眼睛浮肿',
                             isGet:false,
                         },{
-                            index:'drying_and_tingling_lips',
+                            code:'drying_and_tingling_lips',
                             name:'嘴唇干燥',
                             isGet:false,
                         },{
-                            index:'sinus_pressure',
+                            code:'sinus_pressure',
                             name:'鼻窦炎',
                             isGet:false,
                         },{
-                            index:'runny_nose',
+                            code:'runny_nose',
                             name:'流鼻涕',
                             isGet:false,
                         },{
-                            index:'visual_disturbances',
+                            code:'visual_disturbances',
                             name:'视觉障碍',
                             isGet:false,
                         }]
                     },{
-                        index:'back',
+                        code:'back',
                         name:'背部',
                         symp:[{
-                            index:'back_pain',
+                            code:'back_pain',
                             name:'背痛',
                             isGet:false,
                         },{
-                            index:'scurring',
+                            code:'scurring',
                             name:'脱皮',
                             isGet:false,
                         },{
-                            index:'red_spots_over_body',
+                            code:'red_spots_over_body',
                             name:'小疹子',
                             isGet:false,
                         },{
-                            index:'muscle_weakness',
+                            code:'muscle_weakness',
                             name:'肌肉无力',
                             isGet:false,
                         }]
                     },{
-                        index:'arms',
+                        code:'arms',
                         name:'四肢',
                         symp:[{
-                            index:'weakness_in_limbs',
+                            code:'weakness_in_limbs',
                             name:'四肢无力',
                             isGet:false,
                         },{
-                            index:'swollen_extremeties',
+                            code:'swollen_extremeties',
                             name:'四肢肿胀',
                             isGet:false,
                         },{
-                            index:'knee_pain',
+                            code:'knee_pain',
                             name:'膝盖疼痛',
                             isGet:false,
                         },{
-                            index:'prominent_veins_on_calf',
+                            code:'prominent_veins_on_calf',
                             name:'小腿静脉突出',
                             isGet:false,
                         },{
-                            index:'hip_joint_pain',
+                            code:'hip_joint_pain',
                             name:'髋关节疼痛',
                             isGet:false,
                         }]
                     },{
-                        index:'belly',
+                        code:'belly',
                         name:'腹部',
                         symp:[{
-                            index:'abdominal_pain',
+                            code:'abdominal_pain',
                             name:'腹痛',
                             isGet:false,
                         },{
-                            index:'diarrhoea',
+                            code:'diarrhoea',
                             name:'腹泻',
                             isGet:false,
                         },{
-                            index:'swelling_of_stomach',
+                            code:'swelling_of_stomach',
                             name:'胃部肿胀',
                             isGet:false,
                         },{
-                            index:'palpitations',
+                            code:'palpitations',
                             name:'心悸',
                             isGet:false,
                         }]
                     },{
-                        index:'head',
+                        code:'head',
                         name:'头部',
                         symp:[{
-                            index:'dizziness',
+                            code:'dizziness',
                             name:'头晕',
                             isGet:false,
                         },{
-                            index:'depression',
+                            code:'depression',
                             name:'抑郁',
                             isGet:false,
                         }]
                     }
                 ],
-                
+                getted:[
+                ]
+            },
+            predict: {
+                isShow: false,
             }
         }
     },
 
     methods:{
-        swtch(index){
-            for(var i = 0;i<this.symptom.part.length;i++){
-                if(this.symptom.part[i].index != index){
-                    this.symptom.part[i].active = false;
+        submitPredict(){
+            this.symptom.isShow = false;
+            this.step = 2;
+            this.predict.isShow = true;
+        },
+
+        // 选项变化时同步更改tag
+        changeGetted(pindex,sindex){
+            if(this.symptom.part[pindex].symp[sindex].isGet){
+                let newGet = {
+                    name: this.symptom.part[pindex].symp[sindex].name,
+                    code: this.symptom.part[pindex].symp[sindex].code,
+                    p_index: pindex,
+                    s_index: sindex,
+                };
+                this.symptom.getted.push(newGet);
+            }else{
+                for(let g_index in this.symptom.getted){
+                    let g = this.symptom.getted[g_index];
+                    if(g.code == this.symptom.part[pindex].symp[sindex].code){
+                        this.symptom.getted.splice(g_index,1); 
+                    }
                 }
             }
         },
+
+        // 删除tag时同步去除选项
+        delTag(t_code){
+            for(let g_index in this.symptom.getted){
+                let g = this.symptom.getted[g_index];
+                if(g.code == t_code){
+                    let pindex = g.p_index;
+                    let sindex = g.s_index;
+                    this.symptom.part[pindex].symp[sindex].isGet = false;
+                    this.symptom.getted.splice(g_index,1);
+                }
+
+            }
+        }
     }
 }
 
@@ -167,8 +224,19 @@ export default {
     }
 
     #select{
-        width: 50%;
+        width: 70%;
         margin-left: auto;
         margin-right: auto;
+    }
+    
+    #next{
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .tag{
+        margin-top: 5px;
+        margin-bottom: 5px;
+        margin-left: 8px;
     }
 </style>
