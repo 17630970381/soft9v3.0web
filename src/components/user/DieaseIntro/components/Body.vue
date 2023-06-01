e273d13ea50aaf31e632d996dd42a316bf2f7c03
王云飞 <Wangyunfei1203>
2023-06-01 16:38:01 +0800

    r人体

--- src/components/user/DieaseIntro/components/Body.vue
+++ src/components/user/DieaseIntro/components/Body.vue
@@ -2,122 +2,122 @@
   <div id="mychart"></div>
 </template>
 <script>
-import * as echarts from "echarts"
-import svg from "@/assets/people.svg"
+import * as echarts from 'echarts';
+import svg from '@/assets/people.svg';
 export default {
-  name: "",
+  name: '',
   components: {},
   mixins: [],
-  props: {},
+  props: {selectName:String,hoverName:String,},
   data() {
-    return {};
+    return {
+      // selectName: '',
+      // hoverName: '',
+      outName:"",
+      myChart:''
+    };
   },
   computed: {},
-  watch: {},
+  watch: {
+   hoverName(){
+     console.log(this.hoverName)
+     if(this.outName){
+       this.myChart.dispatchAction({
+        type: 'downplay',
+        geoIndex: 0,
+        name: this.outName
+      });
+     }
+     if(this.hoverName){
+       this.outName=this.hoverName
+        this.myChart.dispatchAction({
+        type: 'highlight',
+        // seriesIndex:0,
+        geoIndex: 0,
+        name: this.hoverName
+      });
+     }
+   }
+  },
   mounted() {
     this.initMyChart();
   },
   methods: {
     initMyChart() {
       //   var ROOT_PATH = "server";
-      var chartDom = document.getElementById("mychart");
-      var myChart = echarts.init(chartDom, "dark");
+      var chartDom = document.getElementById('mychart');
+      var myChart = echarts.init(chartDom);
+      this.myChart=myChart
       var option;
-      console.log(myChart);
-
-
-
+      // console.log(myChart);
       var svglink = svg;
 
-        fetch(svglink)
-        .then(response => response.text())
+      fetch(svglink)
+        .then((response) => response.text())
         .then((res) => {
-        //   console.log(res);
-          echarts.registerMap("organ_diagram", { svg: res});
+          //   console.log(res);
+          echarts.registerMap('organ_diagram', { svg: res });
           option = {
-          tooltip: {},
-          geo: {
-            // left: '-10%',
-            // right: "50%",
-            top: "5%",
-
-            map: "organ_diagram",
-            selectedMode: "multiple",
-            emphasis: {
-              focus: "self",
+            // backgroundColor: '#fff', // 设置底色
+            tooltip: {},
+            geo: {
+              // left: '-10%',
+              // right: "50%",
               itemStyle: {
                 color: null,
               },
-              label: {
-                position: "bottom",
-                distance: 0,
-                textBorderColor: "#fff",
-                textBorderWidth: 2,
+              top: '5%',
+              map: 'organ_diagram',
+              selectedMode: 'multiple',
+              emphasis: {
+                focus: 'self',
+                itemStyle: {
+                  color: null,
+                },
+                label: {
+                  position: 'bottom',
+                  distance: 0,
+                  textBorderColor: '#fff',
+                  textBorderWidth: 2,
+                },
+              },
+              blur: {},
+              select: {
+                itemStyle: {
+                  color: '#b50000',
+                  opacity: 1, // 设置元素的透明度为 1，保持不变颜色
+
+                },
+                label: {
+                  show: false,
+                  textBorderColor: '#fff',
+                  textBorderWidth: 2,
+                },
+               
               },
             },
-            blur: {},
-            select: {
-              itemStyle: {
-                color: "#b50205",
-              },
-              label: {
-                show: false,
-                textBorderColor: "#fff",
-                textBorderWidth: 2,
-              },
-            },
-          },
-        //   grid: {
-        //     left: "60%",
-        //     top: "20%",
-        //     bottom: "20%",
-        //   },
-          xAxis: {},
-        //   yAxis: {
-        //     data: [
-        //       "heart",
-        //       "large-intestine",
-        //       "small-intestine",
-        //       "spleen",
-        //       "kidney",
-        //       "lung",
-        //       "liver",
-        //     ],
-        //   },
-        //   series: [
-        //     {
-        //       type: "bar",
-        //       emphasis: {
-        //         focus: "self",
-        //       },
-        //       data: [121, 321, 141, 52, 198, 289, 139],
-        //     },
-        //   ],
-        };
-        myChart.setOption(option);
-        myChart.on("mouseover", { seriesIndex: 0 }, function (event) {
-          myChart.dispatchAction({
-            type: "highlight",
-            geoIndex: 0,
-            name: event.name,
+          };
+          this.myChart.setOption(option);
+          this.myChart.on('mouseover',{ seriesIndex: 0 }, event=> {
+            this.$emit('hover',event.name)
           });
-        });
-        myChart.on("mouseout", { seriesIndex: 0 }, function (event) {
-          myChart.dispatchAction({
-            type: "downplay",
-            geoIndex: 0,
-            name: event.name,
+          this.myChart.on('mouseout',{ seriesIndex: 0 },  event=> {
+            this.$emit('hover','')
           });
+          this.myChart.on('click', event=> {
+            this.$emit('select',event.name)
+          });
+          option && this.myChart.setOption(option);
         });
-        option && myChart.setOption(option);
-      })
     },
   },
 };
 </script>
-<style  scoped>
-#mychart{
- width: 100%;
- height: 100%;
+<style scoped>
+#mychart {
+  width: 140%;
+  height: 100%;
+  transform: translateX(-20%);
+  background-color: aliceblue;
 }
 </style>
--- src/components/user/DieaseIntro/components/Info.vue
+++ src/components/user/DieaseIntro/components/Info.vue
@@ -1,48 +1,81 @@
 <template>
-  <el-collapse v-model="activeNames" @change="handleChange">
-    <el-collapse-item title="一致性 Consistency" name="1">
-      <div>
-        与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；
-      </div>
-      <div>
-        在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。
-      </div>
-    </el-collapse-item>
-    <el-collapse-item title="反馈 Feedback" name="2">
-      <div>
-        控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；
-      </div>
-      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-    </el-collapse-item>
-    <el-collapse-item title="效率 Efficiency" name="3">
-      <div>简化流程：设计简洁直观的操作流程；</div>
-      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-      <div>
-        帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。
-      </div>
-    </el-collapse-item>
-    <el-collapse-item title="可控 Controllability" name="4">
-      <div>
-        用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；
-      </div>
-      <div>
-        结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。
-      </div>
-    </el-collapse-item>
-  </el-collapse>
+  <el-tabs tab-position="left" id="tabs" v-model="activeName" @tab-click="handleClick" type="border-card">
+    <el-tab-pane name="心脏" label="心脏">
+      <div class="content">心脏</div>
+    </el-tab-pane>
+    <el-tab-pane name="肺部" label="肺部">
+      <div class="content">肺部</div>
+    </el-tab-pane>
+    <el-tab-pane name="肝脏" label="肝脏">
+      <div class="content">肝脏</div>
+    </el-tab-pane>
+    <el-tab-pane name="脾脏" label="脾脏">
+      <div class="content">脾脏</div>
+    </el-tab-pane>
+    <el-tab-pane name="肾脏" label="肾脏">
+      <div class="content">肾脏</div>
+    </el-tab-pane>
+    <el-tab-pane name="大肠" label="大肠">
+      <div class="content">大肠</div>
+    </el-tab-pane>
+    <el-tab-pane name="小肠" label="小肠">
+      <div class="content">小肠</div>
+    </el-tab-pane>
+    <el-tab-pane name="简介" label="简介">
+      <div class="content">简介</div>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      activeNames: ["1"],
-    };
-  },
-  methods: {
-    handleChange(val) {
-      console.log(val);
+  export default {
+    props: {selectName:String},
+    data() {
+      return {
+        activeName: '简介',
+        timer:null
+      };
     },
-  },
-};
+    methods: {
+      // 防抖
+      fd(fuc,...args){
+          clearTimeout(this.timer)
+          this.timer = setTimeout(fuc.bind(this,...args),150)
+      },
+      init(){
+        let tab = document.querySelector('.el-tabs__header')
+        // console.log(tab);
+        tab.addEventListener('mouseover',event=>{
+          this.fd((e)=>{
+            this.$emit('hover',e.target.innerText)
+          },event,)
+        })
+        tab.addEventListener('mouseout',event=>{
+         
+          this.fd((e)=>{
+            this.$emit('hover','')
+          },event,)
+            
+          
+        })
+      },
+      handleClick(tab, event) {
+        console.log(tab.$el.id,event);
+      }
+    },
+    mounted(){
+      this.init()
+    },
+    watch:{
+      selectName(){
+        this.activeName=this.selectName?this.selectName:'简介'
+      }
+    }
+  };
 </script>
-<style lang="" scoped></style>
+<style scoped>
+.el-tabs {
+  height: 100%;
+  box-shadow: none;
+  border: none;
+}
+</style>
